46aae3d9b01cf8f5d1f076ef7621d25d74407c20
lingda <260062108@qq.com>
2017-11-14 14:24:39 +0800

    添加gulp插件要求，以及gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+/*
+    利用gulp-sass编译css
+        * requie(): 导入模块
+ */
+
+ var gulp = require('gulp');
+ var sass = require('gulp-sass');
+
+
+ // 创建一个任务
+// 用来编译sass
+gulp.task('compileSass',function(){
+    // 查找文件位置,得到文件流（文件在内存中的状态）
+    gulp.src('./src/sass/*.scss').pipe(sass({outputStyle:'expanded'})).pipe(gulp.dest('./src/css'))//输出到硬盘
+});
+
+// 监听文件的任务
+gulp.task('jtSass',function(){
+    // 监听home.scsss文件
+    // 如果有修改，则执行compileSass任务
+    gulp.watch('./src/sass/*.scss',['compileSass'])
+});
