97efa413fb1656ffc670d9867ca26d325e77aa0b
lingda <260062108@qq.com>
2017-11-16 20:44:23 +0800

    添加header.js，首页用require模块管理js

--- src/js/config.js
+++ src/js/config.js
@@ -6,9 +6,11 @@ require.config({
     paths:{
         jquery:'../lib/JQ/jquery-3.2.1',
         // jqueryUI:'../lib/jquery-ui-1.12.1/jquery-ui'
-        // pldCarousel:'../lib/carousel/jQuery.pldCarousel.js',
+        pldCarousel:'../lib/carousel/jQuery.pldCarousel',
+        header:'header'
     },
-    // shim:{
-    //     pldCarousel:['jquery']
-    // }
+    shim:{
+        pldCarousel:['jquery'],
+        header:['jquery']
+    }
 });
--- src/js/header.js
+++ src/js/header.js
@@ -1,8 +1,30 @@
-(function($){
-    $('.all dl').on('mouseover',function(e){
-        
-        if(e.target.tagName.toLowerCase() == 'a'){
-            $('.san_nav').show();
+;(function($){
+    var $nav_dt= $('.all dl dt');console.log($nav_dt);
+    $nav_dt.on('mouseover',function(e){
+        var tagname = ['dt','h5','p','span','a']
+        if(tagname.indexOf(e.target.tagName.toLowerCase())>0){
+            $('<div/>').addClass('san_nav').html(function(){
+                return `<ul class="clearfix type1">
+                            <h5>婴儿奶粉</h5>
+                            <li><a href="#">幼儿奶粉</a></li>
+                            <li><a href="#">成长奶粉</a></li>
+                        </ul>
+                        <ul class="clearfix brand1">
+                            <h5>品牌</h5>
+                            <li><a href="#">新西兰原装</a></li>
+                            <li><a href="#">爱他美</a></li>
+                            <li><a href="#">牛栏</a></li>
+                            <li><a href="#">雅培小安素</a></li>
+                            <li><a href="#">惠氏</a></li>
+                        </ul>`
+            }).appendTo($(this).closest('dt'));
         }
+        if($(this).index()>3){
+            $('.san_nav').css({
+                top:-($(this).index()-3)*78,
+            })
+        }
+    }).on('mouseleave',function(e){
+        $('.san_nav').remove();
     })
-})(jQuery)
+})(jQuery);
